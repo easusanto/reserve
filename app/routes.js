69fd01f80d98158f8fffd3bacaced6a2888ce818
Estelle Susanto <estellesusanto@Estelles-MacBook-Pro.local>
2016-12-06 04:12:21 -0500

    restaurant side stuff

--- app/routes.js
+++ app/routes.js
@@ -43,23 +43,60 @@ module.exports = function(app, User, Reservation) {
 
     app.post('/get_restaurant_reservations', function(req, res){
         var restaurant_name = req.body.restaurant_name;
-        console.log(restaurant_name);
         Reservation.find({restaurant_name: restaurant_name}, function(err, reservation){
             if(err){
-                console.log("Error: ", err);
+                console.log(err);
                 return res.status(500).send();
             }
             if(!reservation){
                 res.json({ message: 'failure' });
                 return res.status(404).send();
             }
-            console.log("Error: ", err);
-            console.log("Reservation: ", reservation);
             res.reservation = reservation;
             res.json(reservation);
             return res.status(200).send();
         })
     });
+
+
+
+    app.post('/approve_restaurant_reservation', function(req, res) {
+        var confNum = req.body.confNum;
+
+        Reservation.update({'confNum':req.body.confNum},{$set:{"approval":true}}, { upsert: false }, function (err, updated) {
+            if(err){
+                console.log("Error: ", err);
+                return res.status(500).send();
+            }
+            if(!updated){
+                res.json({ message: 'failure' });
+                return res.status(404).send();
+            }
+            res.updated = updated;
+            res.json(updated);
+            return res.status(200).send();
+
+        });
+    });
+
+    app.post('/cancel_approve_restaurant_reservation', function(req, res) {
+    var confNum = req.body.confNum;
+
+    Reservation.update({'confNum':req.body.confNum},{$set:{"approval":false}}, { upsert: false }, function (err, updated) {
+        if(err){
+            console.log("Error: ", err);
+            return res.status(500).send();
+        }
+        if(!updated){
+            res.json({ message: 'failure' });
+            return res.status(404).send();
+        }
+        res.updated = updated;
+        res.json(updated);
+        return res.status(200).send();
+
+    });
+});
 
     app.post('/get_user_reservations', function(req, res){
         var username = req.body.username;
@@ -79,10 +116,14 @@ module.exports = function(app, User, Reservation) {
         })
     });
 
+
+
     app.post('/send_user_reservations', function(req, res) {
+        var confNum = req.body.confNum;
         var restaurant_name = req.body.restaurant_name;
         var username = req.body.username;
         var section_of_venue = req.body.section_of_venue;   //part or all or NA
+        var floor = req.body.floor;
         var catering = req.body.catering;                   //true or false or NA
         var catering_options = req.body.catering_options    //json format
         var date = req.body.date;
@@ -90,19 +131,22 @@ module.exports = function(app, User, Reservation) {
         var end_time = req.body.end_time;
         var number_of_people = req.body.number_of_people;
         var price= {type: Number};
-        var requests = req.body.requests;
+        var approval = req.body.approval;
 
         var newreservation = new Reservation();
+        newreservation.confNum = confNum;
         newreservation.username = username;
         newreservation.restaurant_name = restaurant_name;
         newreservation.section_of_venue = section_of_venue;
+        newreservation.floor = floor;
         newreservation.catering = catering;
         newreservation.catering_options = catering_options;
         newreservation.date = date;
         newreservation.start_time = start_time;
         newreservation.end_time = end_time;
         newreservation.number_of_people = number_of_people;
-        newreservation.requests = requests;
+        newreservation.price = price;
+        newreservation.approval = approval;
 
         console.log("THIS IS MY RESERVATION: ", newreservation);
         newreservation.save(function(err, savedReservation){
--- public/js/controllers/RestaurantMainCtrl.js
+++ public/js/controllers/RestaurantMainCtrl.js
@@ -1,33 +1,140 @@
 var app = angular.module('myApp');
 
 app.controller('RestaurantMainController',
-    ['$scope', '$rootScope', '$location', '$http', 'sharedProperties',
-    function ($scope, $rootScope, $location, $http, sharedProperties) {
+    ['$scope', '$rootScope', '$location', '$http', '$window','sharedProperties',
+    function ($scope, $rootScope, $location, $http, $window, sharedProperties) {
+        $scope.section1Toggle = true;
+        $scope.section2Toggle = false;
 
-        // $scope.getRequests = function () {
-        //     var data1 = {
-        //         restaurant_name: sharedProperties.getProperty(),
-        //     };
+        $scope.single_reservation = "";
+        $scope.reservations = "";
 
-        //     $http({
-        //         url: '/get_restaurant_reservations',
-        //         method: "POST",
-        //         data: JSON.stringify(data1),
-        //         headers: {'Content-Type': 'application/json'}
-        //     }).success(function (data, status, headers, config) {
-        //         $scope.reservations = reformatReservationData(data);
-        //         console.log($scope.reservations);
-        //     }).error(function (data, status, headers, config) {
-        //         console.log("error");
-        //     });
-        // };
+        $scope.get_restaurant_reservations = function (restaurant) {
+            var data1 = {
+                restaurant_name: restaurant,
+            };
 
-        $scope.getProperty = function(){
-            console.log(sharedProperties.getRestaurantName());
+            $http({
+                url: '/get_restaurant_reservations',
+                method: "POST",
+                data: JSON.stringify(data1),
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                console.log(data);
+                $scope.reservations = reformatReservationData(data);
+            }).error(function (data, status, headers, config) {
+                console.log("error");
+            });
+        };
+
+        // $scope.getConfNum = function(){
+        //     var temp = Math.floor(Math.random() * (1000000000 - 1000000 + 1)) + 1000000;
+        //     console.log(temp);
+        // }
+
+        $scope.approve = function (restaurant) {
+            console.log(restaurant);
+
+            console.log(restaurant.confNum);
+            var data1 = 
+            {
+                "confNum": restaurant.confNum
+            }
+
+            $http({
+                url: '/approve_restaurant_reservation',
+                method: "POST",
+                data: JSON.stringify(data1),
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                console.log(data);
+                $scope.single_reservation = reformatReservationData(data);
+                $window.location.reload()
+            }).error(function (data, status, headers, config) {
+                console.log("error");
+            });
+        };
+
+        $scope.cancel_approve = function (restaurant) {
+            var data1 = 
+            {
+                "confNum": restaurant.confNum
+            };
+
+            $http({
+                url: '/cancel_approve_restaurant_reservation',
+                method: "POST",
+                data: JSON.stringify(data1),
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                console.log(data.approval);
+                $scope.single_reservation = reformatReservationData(data);
+                $window.location.reload()
+            }).error(function (data, status, headers, config) {
+                console.log("error");
+            });
+        };
+
+        function reformatReservationData(data){
+            var allReservations = [];
+            for(var i = 0; i < data.length; i++){
+                var reservation = {
+                    confNum: data[i].confNum,
+                    name: data[i].username,
+                    description: data[i].requests,
+                    date: parseDate(data[i].date),
+                    start_time: parseTime(data[i].start_time),
+                    end_time: parseTime(data[i].end_time),
+                    numPeople: data[i].number_of_people,
+                    catering: data[i].catering,
+                    catering_options: data[i].catering_options,
+                    price: data[i].price,
+                    section_of_venue: data[i].section_of_venue,
+                    floor: data[i].floor,
+                    approval: data[i].approval
+                }
+                allReservations[i] = reservation;
+            }
+            return allReservations;
         }
 
-        $scope.click = function(){
-            console.log("clicked!");
+        function parseTime(time){
+            var hour = time.substring(0,2);
+            var minute = time.substring(3, 5);
+            var subscript = "";
+            if(hour > 12){
+                subscript = "pm";
+                hour = Number(hour) - 12;
+                hour = hour.toString();
+            }
+            else {
+                subscript = "am";
+            }
+            return hour + ":" + minute + " " + subscript;
         }
 
+        function parseDate(date){
+            var d = new Date(date);
+            var curr_date = d.getDate();
+            var curr_month = d.getMonth() + 1; //Months are zero based
+            var curr_year = d.getFullYear();
+            return (curr_month + "/" + curr_date + "/" + curr_year);
+        }
+
+        $scope.viewSingle = function(item){
+            $scope.section1Toggle = false;
+            $scope.section2Toggle = true;
+            $scope.single_reservation = item;
+            console.log($scope.single_reservation.name);
+        }
+
+        $scope.back = function(){
+            $scope.section2Toggle = false;
+            $scope.section1Toggle = true;
+        }
+
+        var init = function() {
+            $scope.get_restaurant_reservations('hh');
+        }
+        init();
     }]);
